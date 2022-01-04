09cd2008c138f5def3834402d59b797e7047a8cc
Marco Aurélio <26443642+maureliofs@users.noreply.github.com>
2022-01-03 22:53:19 -0300

    update gitconfig

--- config.sh
+++ config.sh
@@ -4,3 +4,4 @@ stow alacritty/ --target ~/.config/alacritty/
 stow i3blocks/ --target ~/.config/i3blocks/
 stow rofi/ --target ~/.config/rofi/
 stow picom/ --target ~/.config/picom/
+stow git/ --target ~
